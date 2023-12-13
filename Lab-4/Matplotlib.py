4fb3fa99cdb930333e672e17713835779f81bb8f
Ham乙a Wa丂eem <120158214+Humza-Waseem@users.noreply.github.com>
2023-12-13 23:52:27 +0500

    Matplotlib usage

---
+++ Lab-4/Matplotlib.py
@@ -0,0 +1,22 @@
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+x = [1,2,3,4,5]
+y = [2,4,6,7,8]
+
+# to plot the graph 
+plt.plot(x,y,label='redLine',color="red")
+
+plt.title("Its my graph"  , fontdict = {'fontname': 'Times New Roman', 'fontsize':20})
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+
+plt.xticks([1,2,3,4,5])
+plt.yticks([2,4,6,7,8])
+
+plt.legend()
+plt.show()
